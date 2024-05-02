fce143506700cc7ed02a3c84215af0bbd25403a4
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-05-02 15:38:26 +0400

    Your commit message here

---
+++ js/hw-24.js
@@ -0,0 +1,19 @@
+// Задание: 1
+// const listLinkItem = document.querySelectorAll(".list-link__item");
+
+// listLinkItem.forEach((link) => {
+//   if (link.href.startsWith("http://")) {
+//     link.classList.add("list-link");
+//   }
+// });
+// ---------------------------------------------------------------------------
+// Задание: 3
+// const bookList = document.querySelectorAll("li");
+// bookList.forEach((item) => {
+//   item.addEventListener("click", (event) => {
+//     bookList.forEach((book) => {
+//       book.style.backgroundColor = "";
+//     });
+//     event.target.style.backgroundColor = "orange";
+//   });
+// });
